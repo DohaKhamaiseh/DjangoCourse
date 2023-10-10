67edb799f4f83df331412a9f3587314e36ad5b4d
DohaKhamaiseh <https://github.com/DohaKhamaiseh>
2023-10-10 16:22:00 +0300

    The 3 types of Validation

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
+from rest_framework import status
 from watchlist_app.models import Movie
+
+def len_name(value):
+    """
+     its a validators function, I can name it whatever I want
+    """
+    if len(value) < 3:
+             # Note: I can't send a status message too: status=status.HTTP_406_NOT_ACCEPTABLE
+            raise serializers.ValidationError("The Name: Short")
 
 class MovieSerializer(serializers.Serializer):
     
     # read_only mean auto-increasing, not allow to edit
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[len_name])
     description = serializers.CharField()
     active = serializers.BooleanField()
     
@@ -21,3 +30,31 @@ class MovieSerializer(serializers.Serializer):
         instance.save()
         
         return instance
+    
+    # Notes: 
+       # 1. If we have multiple validation(feild and object level) applied the priority is for feild-level validation
+       # 2. If we have multiple validation(feild and validators level) applied the priority is for validators
+    
+    def validate_name(self, value):
+        """
+        a  feild-level validation function to check anything for a specific feild(from the model)
+        the name of the function should be validate_the name of the feild, which is "name" here
+        """
+        # value : value of the name feild
+        if len(value) < 3:
+            # Note: I can't send a status message too: status=status.HTTP_406_NOT_ACCEPTABLE
+            raise serializers.ValidationError("The Name is too Short")
+
+        return value
+    
+    def validate(self,data):
+         """
+        a  object-level validation function to check anything for a specific object(one movie for example)
+        the name of the function should be validate
+        """
+         # data is the values for all feilds for certain object(movie)
+         if data['name'] == data['description']:
+             raise serializers.ValidationError('The name and description of the movie can not be the same')
+         
+         return data
+            
